26ccedd54854c6788104e9013717d95f8ad139a4
Tawheed Anjum <tawheed256@gmail.com>
2025-11-08 12:55:01 +0600

    0xTawheed

---
+++ js/typed.init.js
@@ -0,0 +1,12 @@
+// Typed Text
+var typed = new Typed("#typed", {
+    strings: ["Web Developer", "Full-Stack Developer", "UI/UX Designer"],
+    typeSpeed: 80, // Smooth typing speed
+    backSpeed: 40, // Smooth backspacing
+    backDelay: 1200, // Pause before erasing
+    startDelay: 300, // Small delay before typing starts
+    loop: true, // Infinite loop
+    showCursor: true, // Show the blinking cursor
+    cursorChar: "|", // Cursor symbol
+    smartBackspace: true, // Only backspace what’s necessary
+});
